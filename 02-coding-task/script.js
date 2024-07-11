18b264011f2de4f181cd1ff6b52d9aa7eee5eb75
Marco Michenfelder <mvsmarco@web.de>
2024-07-11 10:30:31 +0200

    first commit

---
+++ 02-coding-task/script.js
@@ -0,0 +1,30 @@
+//task-01
+console.log("task-01: ");
+
+console.log("M" + "a" + "r" + "c" + "o");
+
+//task-02
+console.log("\n task-02: ");
+
+// Error! Make it work
+console.log("Hallo");
+
+// Error! Make it work
+console.log("That doesn't work");
+
+// should return true. Change only the operator to fix it.
+console.log(50 + 2 >= 52);
+
+// should be false. Change only the operator to fix it.
+console.log("333" === 333);
+
+// Wrong result: Expected 555 not 855. Change only the operator to fix it.
+console.log((600 + 510) / 2);
+
+// Wrong result: Expected a Number 4 received 22
+console.log(2 + 2);
+
+//task-03
+console.log("\n task-03: ");
+
+console.log(console.log(25 / 5) === console.log(50 / 10));
