467cc0aacb1b92e2aa86f3614a48339f4ed83b68
Marco Michenfelder <mvsmarco@web.de>
2024-07-16 14:17:41 +0200

    first commit

---
+++ 07-coding-task/script.js
@@ -0,0 +1,17 @@
+// 01 removeItem function
+
+// TODO: Implement the removeItem function
+
+const animals = ["Dog", "Cat", "Lion"];
+console.log(removeItem(animals, 1));
+// result should be: ["Cat", "Lion"]
+
+console.log(animals);
+// result should be still: ["Dog", "Cat", "Lion"]
+
+const fruits = ["Watermelon", "Banana", "Cherry", "Kiwi", "Pineapple", "Apple"];
+console.log(removeItem(fruits, 3));
+// result should be: ["Watermelon", "Banana", "Kiwi", "Pineapple", "Apple"]
+
+console.log(fruits);
+// result should be still: ["Watermelon", "Banana", "Cherry", "Kiwi", "Pineapple", "Apple"]
